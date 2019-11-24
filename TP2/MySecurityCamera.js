00e77dbbf010bb988e045a274ace9ee6bbd0b90a
Pedro Esteves <up201705160@fe.up.pt>
2019-11-24 20:51:04 +0000

    Added comments to MySecurityCamera

--- TP2/MySecurityCamera.js
+++ TP2/MySecurityCamera.js
@@ -2,6 +2,11 @@
  * MySecurityCamera
  */
 class MySecurityCamera {
+    /**
+     * Creates MySecurityCamera class: creates a rectangle and initializes the shaders
+     * @param {CGFscene} scene 
+     * @param {CGFtextureRTT} textureRTT 
+     */
     constructor(scene, textureRTT){
         this.scene = scene;
         this.textureRTT = textureRTT;
@@ -11,8 +16,10 @@ class MySecurityCamera {
         this.initShaders();
     }
 
-    // Initializes the shaders for the security camera
-    initShaders(){
+    /** 
+     *  Initializes the shaders for the security camera
+     */
+     initShaders(){
         this.shader = new CGFshader(
             this.scene.gl,
             "shaders/securityCamera.vert",
@@ -28,6 +35,10 @@ class MySecurityCamera {
         this.shader.setUniformsValues({colorFactor: 1.5});
     }
 
+    /**
+     * Update function that is called every frame
+     * @param {number} t Number of milisseconds since the last update 
+     */
     update(t){
         this.timeFactor += t;
         const speedFactor = this.speed*0.001;
@@ -35,18 +46,37 @@ class MySecurityCamera {
         this.shader.setUniformsValues({timeFactor: shaderValue});
     }
 
+    /**
+     * Updates the scale factor from the security camera shader 
+     * @param {number} newScaleFactor Number of horizontal lines of the security camera
+     */
     updateScaleFactor(newScaleFactor){
         this.shader.setUniformsValues({scaleFactor: newScaleFactor});
     }
 
+    /**
+     * Updates the speed from the security camera shader
+     * @param {number} newSpeed Speed of the security camera animation
+     */
     updateSpeed(newSpeed){
         this.speed = newSpeed;
     }
 
+    /**
+     * Updates the color factor from the security camera shader
+     * @param {number} newColorFactor Color factor: 1.0 means that the 
+     * color of the horizontal lines will be equal to the rest; a color 
+     * factor greater than 1.0 makes the horizontal lines lighter than 
+     * the original color
+     */
     updateColorFactor(newColorFactor){
         this.shader.setUniformsValues({colorFactor: newColorFactor})
     }
 
+    /**
+     * Display function: enables the security camera shader, binds the rendered
+     * texture and displays the rectangle in the bottom right corner of the screen
+     */
     display(){
         this.scene.setActiveShader(this.shader);
             this.textureRTT.bind(0);
