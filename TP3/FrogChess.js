176579659fcb64c9b506c15459b2d749992cd5cc
Pedro Esteves <up201705160@fe.up.pt>
2019-12-27 19:52:36 +0000

    Changed FrogChess.js

--- TP3/FrogChess.js
+++ TP3/FrogChess.js
@@ -4,31 +4,71 @@
  * @param scene - Reference to MyScene object
  */
 class FrogChess extends CGFobject {
-    constructor(scene, board) {
+    constructor(scene, board, gameMode) {
         super(scene);
         this.scene = scene;
 
         // TODO this.board = board;   (passes from menu, could be a lake or normal board)
 
-        this.board = new MyBoard(scene);
+        this.board = board || new MyBoard(scene);
+        
+        this.player = 1; //First Player is the player 1
+        
+        this.gameMode = gameMode || 3;
+
+        this.fillingBoard = true; //In the beggining of the game, players have to fill the board with frogs
+
+        //If serverCall is true, there was a call to the server and no other call will be made 
+        this.serverCall = false;
     }
 
+    getPlayerColor(){
+        if(this.player == 1)
+            return "blue";
+        
+        return "yellow";
+    }
 
     // ---------- GAME LOGIC -----------
 
     // Choosing a fill position, in the beginning of the game
     chooseFillPosition() {
-        serverCpuFillChoose(this.board.pieces, this.handlePosition);
+        if(!this.serverCall){
+            serverCpuFillChoose(this.board.pieces, this.handlePosition);
+            this.serverCall = true;
+        }        
+        
+        if(this.next_position === []){
+            this.fillingBoard = false;
+            return;
+        }
+        
+        if(this.next_position === undefined)
+            return;
+
+        this.board.setPiecePosition(this.next_position, this.getPlayerColor());
+        this.serverCall = false;
     }
 
     // Generates possible fill positions
     getFillPositions() {
-        serverValidFillPositions(this.board.pieces, this.handlePositions);
+        if(!this.serverCall){
+            serverValidFillPositions(this.board.pieces, this.handlePositions);
+            this.serverCall = true;
+        }
+
+        if(this.handlePosition === []){
+            this.fillingBoard = false;
+            return;
+        }
     }
 
     // Generates possible jump positions
     getJumpPositions(position) {
-        serverValidJumpPosition(this.board.pieces, position, this.handlePositions);
+        if(!this.serverCall){
+            serverValidJumpPosition(this.board.pieces, position, this.handlePositions);
+            this.serverCall = true;
+        }
     }
 
 
@@ -38,7 +78,7 @@ class FrogChess extends CGFobject {
     handlePosition(data) {
         let position = JSON.parse(data.target.response);
         this.next_position = position;
-        console.log(position);
+        console.log(this.next_position);
     }
 
     // Parses an array of positions [ [x,y], [x, y], ...] from the request
@@ -55,10 +95,29 @@ class FrogChess extends CGFobject {
         console.log(board);
     }
 
-
-    
+    update(t){
+        if(this.fillingBoard){
+            switch(this.gameMode){
+                case 1: //Players are both human
+                    this.getFillPositions();
+                    break;
+                case 2: //Player 1 -> human; Player 2 -> CPU
+                    if(player == 1){
+                        this.getFillPositions();
+                    }else{
+                        this.chooseFillPosition();
+                    }
+                    break;
+                case 3: //Players are both CPU
+                    this.chooseFillPosition();
+                    break;
+                default: 
+                    return;
+            }
+        }
+    }
 
     display() {
-        
+        this.board.display();
     }
 }
